62f1958c083f353ee738da7dfe77a151e60e79e6
Kendall Jackson <kendallkj00@gmail.com>
2024-05-14 12:43:06 -0400

    fix: slight reduction in size, border color

--- pages/bumper-replacement.vue
+++ pages/bumper-replacement.vue
@@ -14,152 +14,152 @@ export default {
           name: '188" K55 Rubber - 3 Pcs',
           img: {
             src: '188-K55-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" K55 Carom Rubber - 3 Pcs',
           img: {
             src: '118-K55-carom-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" K66 Rubber - 3 Pcs',
           img: {
             src: '118-K66-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '72" Canadian Step Rubber - 6 Pcs',
           img: {
             src:'72-canadian-step-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" A48 Rail Rubber - 6 Pcs',
           img: {
             src:'48-A48-rail-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" Artemis Intercontinental Pool No. 66 Rubber - 6pcs',
           img: {
             src:'48-artemis-intercontinental-no-66-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" K55 Standard Rubber - 6 Pcs',
           img: {
             src:'48-K55-standard-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" Artemis Pool No. 66 Rubber - 6 Pcs',
           img: {
             src:'48-artemis-no-66-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" U23 Standard Rubber - 6 Pcs',
           img: {
             src:'48-U23-standard-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '42" Century Rubber - 6 Pcs',
           img: {
             src:'42-century-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" K66 Standard Rubber - 6 Pcs',
           img: {
             src:'48-K66-standard-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" U118 Carom Rubber - 3 Pcs',
           img: {
             src:'118-U118-carom-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" Artemis No. 79 Carom Rubber - 3 Pcs',
           img: {
             src:'118-artemis-no-79-carom-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" Artemis No. 37 Carom Rubber - 3 Pcs',
           img: {
             src:'118-artemis-no-37-carom-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" #313 Small Rubber - 6 Pcs',
           img: {
             src:'48-313-small-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '9\' Olhausen Accufast Cushions',
           img: {
             src:'9-olhausen-cushions.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '118" Artemis Intercontinental No. 66 Carom Rubber - 3 Pcs',
           img: {
             src:'118-artemis-intercontinental-no-66-carom-rubber-3.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: 'K55 Snooker Bends - 12 Pcs',
           img: {
             src:'K55-snooker-bends-12.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
         {
           name: '48" #818 Small Rubber - 6 Pcs',
           img: {
             src:'48-818-small-rubber-6.jpg',
-            width: 350,
-            height: 315
+            width: 320,
+            height: 288
           }
         },
       ]
