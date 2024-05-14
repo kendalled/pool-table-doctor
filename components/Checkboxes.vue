edc211c1f0499a0da5a7d4d92cc5578f2e3a5104
Kendall Jackson <kendallkj00@gmail.com>
2024-05-14 11:07:44 -0400

    fix: make labels clickable, work on spacing

--- components/Checkboxes.vue
+++ components/Checkboxes.vue
@@ -1,17 +1,16 @@
 <template>
   <fieldset>
-    <!-- todo: make whole label clickable -->
     <legend class="sr-only">Services</legend>
-    <label for="email" class="block text-sm font-semibold leading-6 text-gray-900 mb-2">Services</label>
-    <div class="space-y-2 grid md:grid-cols-2 gap-x-2 gap-y-2">
+    <label for="services" class="block text-sm font-semibold leading-6 text-gray-900 mb-4">Services</label>
+    <!-- todo: fix spacing -->
+    <div id="services" class="space-y-2 grid md:grid-cols-2 gap-x-2 gap-y-2">
       <!-- start checkbox item-->
       <div v-for="service in serviceList" class="relative flex items-center">
         <div class="flex h-6 items-center">
           <input :id="service.id" :aria-describedby="service.id + '-description'" :name="service.id" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-green-600 focus:ring-green-600">
         </div>
         <div class="ml-3 text-sm leading-6">
-          <label :id="service.id" class="font-medium text-gray-900 hidden">{{ service.name }}</label>
-          <label :id="service.id + '-description'" class="text-gray-700">{{ service.name }}<span class="sr-only"><!-- was hidden content --></span></label>
+          <label :id="service.id + '-description'" :for="service.id" class="text-gray-700">{{ service.name }}<span class="sr-only"><!-- was hidden content --></span></label>
         </div>
       </div>
       <!-- end checkbox item -->
