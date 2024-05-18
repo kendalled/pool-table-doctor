5124e7615d88026f732d011362a0c2124cbd9844
Kendall Jackson <kendallkj00@gmail.com>
2024-05-18 16:41:23 -0400

    feat: add sample meta title and desc

--- pages/index.vue
+++ pages/index.vue
@@ -10,3 +10,18 @@
     <FeatureList />
   </div>
 </template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'The Pool Table Doctor: Moves, Repairs, and Service Work',
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: 'The Pool Table Doctor has been happily serving Central Florida since 1994. We offer industry-leading pool table repairs, refelting, and more. Contact us today for a free quote, and find out why clients big and small trust The Pool Table Doctor.'
+      }]
+    }
+  }
+}
+</script>
