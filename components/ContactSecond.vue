0677ef88a334b0d1b932ebc5a2052e870e3de6fd
Kendall Jackson <kendallkj00@gmail.com>
2024-05-25 18:03:08 -0400

    fix: iframe title

--- components/ContactSecond.vue
+++ components/ContactSecond.vue
@@ -45,7 +45,7 @@
         </div>
         <!-- start google maps embed -->
         <div class="h-auto w-auto bg-green-900 rounded-md">
-          <iframe class="mt-20 lg:-mb-20 opacity-100 h-96 w-full rounded-md" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJGQ6d2spv54gRVU2RIhzlE3A&key=AIzaSyBh8zoTxTInRHEaMjnhRAt8IpNxnoqubbs"></iframe>
+          <iframe class="mt-20 lg:-mb-20 opacity-100 h-96 w-full rounded-md" title="Google Maps Embed" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJGQ6d2spv54gRVU2RIhzlE3A&key=AIzaSyBh8zoTxTInRHEaMjnhRAt8IpNxnoqubbs"></iframe>
         </div>
         <!-- end google maps embed -->
       </div>
