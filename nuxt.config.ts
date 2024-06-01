0368bcd1675ce7ecf582450acc08eff35ea258d1
Kendall Jackson <kendallkj00@gmail.com>
2024-06-01 16:39:30 -0400

    feat: enable google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
     families: {
       Inter: '300..900'
     }
-}], "@nuxtjs/seo"],
+}], "@nuxtjs/seo", "nuxt-gtag"],
   site: {
     url: 'https://pooltabledoctor.com',
     name: 'The Pool Table Doctor',
     description: 'The Pool Table Doctor has been happily serving Central Florida since 1994. We offer industry-leading pool table repairs, refelting, and more. Contact us today for a free quote, and find out why clients big and small trust The Pool Table Doctor.',
     defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  },
+  gtag: {
+    id: 'G-SD187W76SS'
   },
   app: {
     head: {  
