7b77fa7b939b7dde7b4fa5eebbbaf6cb6a1f010e
Kendall Jackson <kendallkj00@gmail.com>
2024-11-05 11:48:31 -0500

    fix: align content padding with rest of page
    Take off redundant px-6, fix alt tags and image descriptions

--- components/ImageGrid.vue
+++ components/ImageGrid.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mx-auto mt-6">
+  <div class="grid grid-cols-2 md:grid-cols-3 gap-x-12 gap-y-8 mx-auto mt-6">
+    <!-- TODO: rethink spacing on image grid, make images clickable -->
     <div v-for="img in images">
-        <img class="shadow-md h-auto max-w-full w-80 rounded-lg mx-auto" :src="img.src" :alt="img.alt">
-        <p class="mt-2 text-gray-700 mx-auto w-80">{{img.text}}</p>
+        <img class="shadow-md h-auto max-w-full w-full rounded-lg mx-auto" :src="img.src" :alt="img.alt">
+        <p class="mt-2 text-gray-700 mx-auto w-full">{{img.text}}</p>
     </div>
   </div>
 </template>
@@ -14,33 +15,33 @@ export default {
       images: [
         {
           src:'/bumper/examples/cushion-repair-hard.jpg',
-          alt: '',
+          alt: 'Pool table bumper damaged due to weather',
           text: 'Cushion in an uncontrolled temperature environment.'
         },
         {
           src:'/bumper/examples/cushion-repair-soft.jpg',
-          alt: '',
+          alt: 'Pool table bumper damaged due to years of use',
           text: 'Example of continuous play over many years.'
         },
         {
           src:'/bumper/examples/cushion-repair-dryrot.jpg',
-          alt: '',
-          text: 'Cushion in an uncontrolled temperature environment.'
+          alt: 'Additional temperature damage to a pool table bumper',
+          text: 'Cushion cracks due to humidity and temperature.'
         },
         {
           src:'/bumper/examples/bumper1.jpg',
-          alt: '',
+          alt: 'Removal of a damaged bumper cushion',
           text: 'Damaged cushion removal.'
         },
         {
           src:'/bumper/examples/bumper2.jpg',
-          alt: '',
+          alt: 'Sanding off and leveling a bumper',
           text: 'Bumper sand off and leveling.'
         },
         {
           src:'/bumper/examples/bumper3.jpg',
-          alt: '',
-          text: 'Bumper & cushion is like new.'
+          alt: 'Finished result, a repaired pool table bumper',
+          text: 'Bumper & cushion is like new!'
         }
       ]
     }
--- pages/bumper-replacement.vue
+++ pages/bumper-replacement.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="space-y-6 max-w-7xl px-6 sm:px-8 mt-6 w-full mx-auto">
     <!-- todo: refactor to fit old content using vue slots, slightly shrink image sizes when expanded -->
-    <div class="rounded-md bg-white px-6 py-8">
-      <h2 class="text-xl md:text-3xl font-bold text-green-900 mb-4 px-6">The bumper and cushion repair process</h2>
-      <p class="text-gray-800 px-6">
+    <div class="rounded-md bg-white px-6 py-8 sm:px-8">
+      <h2 class="text-xl md:text-3xl font-bold text-green-900 mb-4">The bumper and cushion repair process</h2>
+      <p class="text-gray-800">
         Over time, cushion rubber becomes hard and/or dry rotted. This is caused mostly by pool tables being set up in a garage, porch, or sunlight beaming through the windows over time without cover. Other times, cushion rubber can do the exact opposite and become squishy like a stress ball. This happens when the pool table has been in a controlled climate but played on for a long time, such as tables in a pool hall. No matter what the circumstance, we can fix it!
       </p>
       <ImageGrid />
     </div>
-    <ClothPicker :cloths="bumperChoices" prefix="/bumper/" label="Rubber Cushion Selection" desc="Over time, cushion rubber becomes hard and/or dry rotted. This is caused mostly by pool tables being set up in a garage, porch, or sunlight beaming through the windows over time without cover. Other times, cushion rubber can do the exact opposite and become squishy like a stress ball. This happens when the pool table has been in a controlled climate but played on for a long time, such as tables in a pool hall. No matter what the circumstance, we can fix it!" />
+    <ClothPicker :cloths="bumperChoices" prefix="/bumper/" label="Rubber Cushion Selection" desc="Regardless of the cause or condition, we have a range of rubber cushions to restore your table to peak performance. Tournament-grade bumpers are essential for your table to play properly. Our certified technicians can install a new bumper in no time, and with the most competitive pricing in the area, we can't be beat. Explore our selection below, and let us help you bring your pool table back to its best shape!" />
   </div>
 </template>
 
