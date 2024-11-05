ab9034045ec5ce9f63e28683fc646bf6c66bbef1
Kendall Jackson <kendallkj00@gmail.com>
2024-11-05 12:05:48 -0500

    fix: seo improvements to copy

--- components/HeroTwo.vue
+++ components/HeroTwo.vue
@@ -7,8 +7,8 @@
           <div class="text-center">
             <div class="container px-4 mx-auto">
               <div class="max-w-4xl mx-auto text-center">
-                <span class="text-gray-200 font-semibold uppercase tracking-widest">Experts since 1994</span>
-                <h1 class="mt-8 mb-6 text-2xl md:text-4xl lg:text-5xl font-bold text-gray-50">Pool table installations, moves, refelting, and repairs done right.</h1>
+                <span class="text-gray-200 font-semibold uppercase tracking-widest">Orlando &middot; Tampa &middot; Experts since 1994</span>
+                <h1 class="mt-8 mb-6 text-2xl md:text-4xl lg:text-5xl font-bold text-gray-50">Pool table moves, installations, refelting, and repairs done right.</h1>
                 <p class="max-w-3xl mx-auto mb-10 md:text-lg text-gray-100">
                   Count on the experts who've been perfecting the game since 1994. From installations, moves, and repairs, we're your Central Florida specialists for pool table perfection.
                 </p>
